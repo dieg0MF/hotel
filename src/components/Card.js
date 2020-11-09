f4542dc12b847f9504970d136515183f1acdf844
dieg0MF <65663780+dieg0MF@users.noreply.github.com>
2020-11-09 18:16:07 -0300

    showPrice fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,46 @@
-
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMapMarkerAlt ,faBed,faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faBed,
+  faDollarSign,
+} from "@fortawesome/free-solid-svg-icons";
 
+function Card(props) {
+  const { data, index } = props;
 
- function Card(props) {
-  const { data,index } = props;
+  const mapMarker = <FontAwesomeIcon icon={faMapMarkerAlt} />;
+  const Bed = <FontAwesomeIcon icon={faBed} />;
+  const Dollar = <FontAwesomeIcon icon={faDollarSign} />;
 
-
-  const mapMarker = <FontAwesomeIcon icon={faMapMarkerAlt} />
-  const Bed = <FontAwesomeIcon icon={faBed} />
-  const Dollar = <FontAwesomeIcon icon={faDollarSign} />
-
-  
+  console.log("data.item.price", data.item.price);
   return (
- 
     <div className="card" key={index}>
       <img src={data.item.photo} alt="" className="hotel__image" />
       <div className="info">
         <h4>{data.item.name}</h4>
         <p>{data.item.description}</p>
-        <div className="ubicacion">{mapMarker}
+        <div className="ubicacion">
+          {mapMarker}
           {data.item.city} , {data.item.country}
         </div>
         <div className="habprecio">
-          <div className="habitaciones">{Bed}{data.item.rooms} Habitaciones</div>
-          <div className="precio">   {Dollar}
-        <i
-          className={"fas fa-dollar-sign dolar number" + (5 - data.item.price)}
-        ></i>
-        <i
-          className={"fas fa-dollar-sign dolar number" + (6 - data.item.price)}
-        ></i>
-        <i
-          className={"fas fa-dollar-sign dolar number" + (7 - data.item.price)}
-        ></i></div>
+          <div className="habitaciones">
+            {Bed}
+            {data.item.rooms} Habitaciones
+          </div>
+          <div className="precio">
+            <div className=" dolar number1">{Dollar} </div>
+            <div className={" dolar number" + (5 - data.item.price)}>
+              {Dollar}{" "}
+            </div>
+            <div className={"dolar number" + (6 - data.item.price)}>
+              {Dollar}{" "}
+            </div>
+            <div className={"dolar number" + (7 - data.item.price)}>
+              {Dollar}{" "}
+            </div>
+          </div>
         </div>
       </div>
       <div className="contenedorBoton">
@@ -42,8 +48,7 @@ import { faMapMarkerAlt ,faBed,faDollarSign } from '@fortawesome/free-solid-svg-
           <button>reservar</button>
         </div>
       </div>
-      </div>
-    
+    </div>
   );
 }
-export default Card
+export default Card;
